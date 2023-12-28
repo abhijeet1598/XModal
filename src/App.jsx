6bdb21dbbae62f05eb29c5d671a7be3189e37a41
Abhijeet Karmakar <abhijeetkarmakar150@gmail.com>
2023-12-28 21:45:09 +0530

    XModal

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React, { useState } from "react";
 import "./App.css";
-import Modal from "./components/Modal";
+import Modal2 from "./components/Modal2";
 
-function App() {
+const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <>
-      <Modal />
-    </>
+    <div className="App">
+      <h1>User Details Modal</h1>
+      <button onClick={openModal} className="submit-button">
+        Open Form
+      </button>
+      {isOpen && <Modal2 closeModal={closeModal} />}
+    </div>
   );
-}
+};
 
 export default App;
